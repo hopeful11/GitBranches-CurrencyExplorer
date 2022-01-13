c8c594560337d300f9b342c4c4da1a53972fc2d1
hopeful11 <hhfirdevs@gmail.com>
2022-01-13 18:51:52 +0100

    adds greet_user function

---
+++ greet_user.py
@@ -0,0 +1,6 @@
+def greet_and_get_currency():
+    
+    print("Welcome to CurrencyExplorer App. Please provide your currency of choice and will fetch the latest date for you")
+    currency=input("Currency:")
+
+    return currency.upper()
